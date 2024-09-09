eaf8d3b1150da96843d499a79fdf85d751dce2b6
Caitlyn <clrutledge2@crimson.ua.edu>
2024-09-09 16:27:18 +0100

    Round

--- obj/Debug/net8.0/mis-221-lab-2-clrutledge2.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-2-clrutledge2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-2-clrutledge2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6beb5fcdfcf434119a0b4b5205353d107cac576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0795dfc4ceb672e896e1a32d5f932ca983f6e84f")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-2-clrutledge2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-2-clrutledge2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
